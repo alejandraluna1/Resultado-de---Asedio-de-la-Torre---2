830abb5ee670997b42dd659b2ee89a5659124088
alejandraluna1 <81326858+alejandraluna1@users.noreply.github.com>
2021-04-09 22:04:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
  
-  //level one
+  //nivel uno
   block1 = new Block(300,275,30,40);
   block2 = new Block(330,275,30,40);
   block3 = new Block(360,275,30,40);
@@ -29,34 +29,34 @@ function setup() {
   block5 = new Block(420,275,30,40);
   block6 = new Block(450,275,30,40);
   block7 = new Block(480,275,30,40);
-  //level two
+  //nivel dos
   block8 = new Block(330,235,30,40);
   block9 = new Block(360,235,30,40);
   block10 = new Block(390,235,30,40);
   block11 = new Block(420,235,30,40);
   block12 = new Block(450,235,30,40);
-  //level three
+  //nivel tres
   block13 = new Block(360,195,30,40);
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
-  //top
+  //parte superior
   block16 = new Block(390,155,30,40);
 
-  //set 2 for second stand
-  //level one
+  //conjunto 2 para la segunda posición
+  //nivel uno
   blocks1 = new Block(640,175,30,40);
   blocks2 = new Block(670,175,30,40);
   blocks3 = new Block(700,175,30,40);
   blocks4 = new Block(730,175,30,40);
   blocks5 = new Block(760,175,30,40);
-  //level two
+  //nivel dos
   blocks6 = new Block(670,135,30,40);
   blocks7 = new Block(700,135,30,40);
   blocks8 = new Block(730,135,30,40);
-  //top
+  //parte superior
   blocks9 = new Block(700,95,30,40);
 
-  //polygon holder with slings
+  //soporte poligonal con la resortera
   polygon = Bodies.circle(50,200,20);
   World.add(world,polygon);
   
@@ -66,13 +66,13 @@ function setup() {
 function draw() {
   background(56,44,44); 
  
-  //Engine.update(engine);
+  //Engine.update(motor);
  
   textSize(20);
   fill("lightyellow");
-  text("Drag the polygon to destroy the blocks",300,30);
+  text("Arrastra el polígono para destruir los bloques",300,30);
   textSize(10);
-  text("Press Space to get a second Chance to Play!!",650 ,350);
+  text("¡Presiona la Barra Espaciadora para tener una segunda oportunidad de jugar!",650 ,350);
   ground.display();
   stand1.display();
   stand2.display();
